079613f0e2ebe0fc90f9f1b142ec6f7ecee8203a
Christian <jcromerohdz@gmail.com>
2016-07-22 12:21:01 -0700

    Deploying to heroku

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chris/Documents/Proyectos/heroku_deploy/heroku_app/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
